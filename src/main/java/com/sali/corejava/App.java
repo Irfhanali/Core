7b559f79a9333055f35ea5163daad5aa2752d12b
sali <sali@sali-WX-1.corp.adobe.com>
2018-12-07 03:41:06 +0530

    intial version

---
+++ src/main/java/com/sali/corejava/App.java
@@ -0,0 +1,15 @@
+package com.sali.corejava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Irfhan Ali Hello World!" );
+        
+       System.out.println("new test");
+    }
+}
